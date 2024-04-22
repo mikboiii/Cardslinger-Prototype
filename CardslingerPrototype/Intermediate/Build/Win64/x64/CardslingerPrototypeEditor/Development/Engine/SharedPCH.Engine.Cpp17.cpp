5a5a95ddedddf95cc2659cdbff459ad31a78fed6
mikboiii <mikolajgreszta@gmail.com>
2024-04-22 14:14:41 +0100

    Added Empty Unreal Project

---
+++ CardslingerPrototype/Intermediate/Build/Win64/x64/CardslingerPrototypeEditor/Development/Engine/SharedPCH.Engine.Cpp17.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.38.33135
+#include "C:/Users/Mikolaj/Documents/Unreal Projects/Cardslinger/CardslingerPrototype/Intermediate/Build/Win64/x64/CardslingerPrototypeEditor/Development/Engine/SharedPCH.Engine.Cpp17.h"
---
+++ CardslingerPrototype/Intermediate/Build/Win64/x64/CardslingerPrototypeEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "C:/Users/Mikolaj/Documents/Unreal Projects/Cardslinger/CardslingerPrototype/Intermediate/Build/Win64/x64/CardslingerPrototypeEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
