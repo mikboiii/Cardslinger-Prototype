c33ee310e2e9871de1ad2bec8c3ddb2cf0bb1a8b
mikboiii <mikolajgreszta@gmail.com>
2024-07-03 15:39:12 +0100

    Refactored card widgets

---
+++ CardslingerPrototype/Intermediate/Build/Win64/UnrealEditor/Inc/CardslingerPrototype/UHT/CardUIWidget.generated.h
@@ -0,0 +1,66 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "CardUIWidget.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CARDSLINGERPROTOTYPE_CardUIWidget_generated_h
+#error "CardUIWidget.generated.h already included, missing '#pragma once' in CardUIWidget.h"
+#endif
+#define CARDSLINGERPROTOTYPE_CardUIWidget_generated_h
+
+#define FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_SPARSE_DATA
+#define FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_ACCESSORS
+#define FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUCardUIWidget(); \
+	friend struct Z_Construct_UClass_UCardUIWidget_Statics; \
+public: \
+	DECLARE_CLASS(UCardUIWidget, UUserWidget, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/CardslingerPrototype"), NO_API) \
+	DECLARE_SERIALIZER(UCardUIWidget)
+
+
+#define FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UCardUIWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UCardUIWidget(UCardUIWidget&&); \
+	NO_API UCardUIWidget(const UCardUIWidget&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UCardUIWidget); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UCardUIWidget); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UCardUIWidget) \
+	NO_API virtual ~UCardUIWidget();
+
+
+#define FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_12_PROLOG
+#define FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_SPARSE_DATA \
+	FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_ACCESSORS \
+	FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CARDSLINGERPROTOTYPE_API UClass* StaticClass<class UCardUIWidget>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_Mikolaj_Documents_Unreal_Projects_Cardslinger_Prototype_CardslingerPrototype_Source_CardslingerPrototype_CardUIWidget_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- CardslingerPrototype/Intermediate/Build/Win64/UnrealEditor/Inc/CardslingerPrototype/UHT/CardslingerPrototype.init.gen.cpp
+++ CardslingerPrototype/Intermediate/Build/Win64/UnrealEditor/Inc/CardslingerPrototype/UHT/CardslingerPrototype.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeCardslingerPrototype_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xD1165D3F,
-				0xE8A824D7,
+				0x66D5B25B,
+				0x9B242271,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_CardslingerPrototype.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_CardslingerPrototype.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_CardslingerPrototype(Z_Construct_UPackage__Script_CardslingerPrototype, TEXT("/Script/CardslingerPrototype"), Z_Registration_Info_UPackage__Script_CardslingerPrototype, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xD1165D3F, 0xE8A824D7));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_CardslingerPrototype(Z_Construct_UPackage__Script_CardslingerPrototype, TEXT("/Script/CardslingerPrototype"), Z_Registration_Info_UPackage__Script_CardslingerPrototype, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x66D5B25B, 0x9B242271));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
