357686eeb54d9572edfde79fc17efd52c58badf2
mikboiii <mikolajgreszta@gmail.com>
2024-10-15 16:13:53 +0100

    Created c++ and bp class for reflection card

---
+++ CardslingerPrototype/Source/CardslingerPrototype/ReflectionCard.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ReflectionCard.h"
+#include "Kismet/GameplayStatics.h"
+#include "BaseCharacterClass.h"
+
+void AReflectionCard::CardEffect(class ACardDeck* CardDeck, FVector Direction, FVector Target, AActor* TargetActor)
+{
+    
+}
