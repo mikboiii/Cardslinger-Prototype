46793917d86f5abdd69c3e34bcada61018326722
mikboiii <mikolajgreszta@gmail.com>
2024-11-05 15:00:53 +0000

    Added deflection vignette screen
    Fixed forward declaration in EnemyProjectile
    Added deflection vignette event

--- CardslingerPrototype/Intermediate/Build/Win64/UnrealEditor/Inc/CardslingerPrototype/UHT/CardslingerPrototype.init.gen.cpp
+++ CardslingerPrototype/Intermediate/Build/Win64/UnrealEditor/Inc/CardslingerPrototype/UHT/CardslingerPrototype.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeCardslingerPrototype_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xC5386561,
+				0x805DACC6,
 				0x8EC289D6,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeCardslingerPrototype_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_CardslingerPrototype.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_CardslingerPrototype(Z_Construct_UPackage__Script_CardslingerPrototype, TEXT("/Script/CardslingerPrototype"), Z_Registration_Info_UPackage__Script_CardslingerPrototype, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xC5386561, 0x8EC289D6));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_CardslingerPrototype(Z_Construct_UPackage__Script_CardslingerPrototype, TEXT("/Script/CardslingerPrototype"), Z_Registration_Info_UPackage__Script_CardslingerPrototype, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x805DACC6, 0x8EC289D6));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
