2678122542a4c78988a9b07d79beddc91f170974
mikboiii <mikolajgreszta@gmail.com>
2026-02-04 11:19:00 +0000

    Created new class for pickups

---
+++ CardslingerPrototype/Source/CardslingerPrototype/Public/PickupBase.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PickupBase.generated.h"
+
+UCLASS()
+class CARDSLINGERPROTOTYPE_API APickupBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickupBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void OnPickUp();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
